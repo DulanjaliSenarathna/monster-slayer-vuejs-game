6ac97168e6d53de4c2d6b9052b86bbd02dffc0c0
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-09 12:28:08 +0530

    Adding a "Heal" Functionality

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ const app = Vue.createApp({
         },
         mayUseSpecialAttack(){
             return this.currentRound % 3 !==0;
-        }
+        },
+        // mayUseHeal(){
+        //     return this.currentRound % 3 !==0;
+        // }
     },
     methods:{
         attackMonster(){
@@ -37,8 +40,18 @@ const app = Vue.createApp({
             const attackValue = getRandomValue(10,25);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
-        }
-    }
+        },
+        healPlayer(){
+            this.currentRound++;
+            const healValue = getRandomValue(8,20);
+            if(this.playerHealth + healValue >100){
+                this.playerHealth = 100;
+            }else{
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();   
+        },
+    },
 });
 
 app.mount("#game")
