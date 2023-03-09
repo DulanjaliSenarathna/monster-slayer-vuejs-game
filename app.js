fc323d8022846bd9ae32f527a968bfcfdc7b5e92
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-09 10:16:52 +0530

    Adding a "Special Attack"

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
     data(){
         return{
             playerHealth:100,
-            monsterHealth:100
+            monsterHealth:100,
+            currentRound:0
         }
     },
     computed:{
@@ -15,10 +16,14 @@ const app = Vue.createApp({
         },
         playerBarStyles(){
             return {width:this.playerHealth + '%'}
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound % 3 !==0;
         }
     },
     methods:{
         attackMonster(){
+          this.currentRound++;
           const attackValue = getRandomValue(5,12);
           this.monsterHealth -= attackValue;
           this.attackPlayer();
@@ -26,6 +31,12 @@ const app = Vue.createApp({
         attackPlayer(){
             const attackValue =  getRandomValue(8,15);
             this.playerHealth -= attackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const attackValue = getRandomValue(10,25);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
         }
     }
 });
